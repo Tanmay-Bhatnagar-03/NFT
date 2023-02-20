5bbea120c25e2882eb05c3569bb6e870dc41987f
Tanmay <106529298+Tanmay-Bhatnagar-03@users.noreply.github.com>
2023-02-20 20:18:26 +0530

    Update

--- hardhat-tutorial/contracts/CryptoDevs.sol
+++ hardhat-tutorial/contracts/CryptoDevs.sol
@@ -10,7 +10,7 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
       * @dev _baseTokenURI for computing {tokenURI}. If set, the resulting URI for each
       * token will be the concatenation of the `baseURI` and the `tokenId`.
       */
-    string _baseTokenURI;
+    string _baseTokenURI; 
 
     //  _price is the price of one Crypto Dev NFT
     uint256 public _price = 0.01 ether;
@@ -44,7 +44,7 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
       * Constructor for Crypto Devs takes in the baseURI to set _baseTokenURI for the collection.
       * It also initializes an instance of whitelist interface.
       */
-    constructor (string memory baseURI, address whitelistContract) ERC721("Crypto Devs", "CD") {
+    constructor (string memory baseURI, address whitelistContract) ERC721("Brahmaputra NFTs", "BPH") {
         _baseTokenURI = baseURI;
         whitelist = IWhitelist(whitelistContract);
     }
